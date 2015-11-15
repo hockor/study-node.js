e4ae6ba6a2ae5f56bdd100b2646de078ed739559
hockor <809955044@qq.com>
2015-11-15 19:29:21 +0800

    API files

---
+++ API/Buffer/3.js
@@ -0,0 +1,12 @@
+﻿
+
+var bf = new Buffer('hockor');
+console.log( bf.toString() );
+console.log( bf.toString('utf-8', 1, 3) );
+
+var bf2 = new Buffer('中国');
+console.log(bf2);
+console.log( bf2.toString('utf-8', 1) );
+
+
+console.log( bf.toJSON() );
